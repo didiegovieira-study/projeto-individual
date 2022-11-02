1cfe92761b1d80ac4268dc0c3c5d3f3b42705b4d
didiegovieira <diego.vieira@sptech.school>
2022-11-02 10:20:16 -0300

    att login and register

--- site/public/js/login.js
+++ site/public/js/login.js
@@ -1,10 +1,4 @@
 
-    setInterval(function() {background.style.backgroundImage  = "url('assets/img/login/screen-messi.jpg')"}, 4000);
-    setInterval(function() {background.style.backgroundImage  = "url('assets/img/login/screen-cr7.jpg')"}, 8000);
-    setInterval(function() {background.style.backgroundImage  = "url('assets/img/login/screen-neymar.jpg')"}, 12000);
-
-
-
     function entrar() {
         aguardar();
 
