dd7075cd93daf3f99ecae6275b8cc825935d7fbb
didiegovieira <diego.vieira@sptech.school>
2022-11-09 14:17:58 -0300

    att web-data-viz

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -65,6 +65,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var sobre = req.body.sobreServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -73,6 +74,8 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
+    } else if (sobre == undefined) {
+        res.status(400).send("Sua sobrenome está undefined!");
     } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
