d2cf25fed694525045268c0f97984bd33e866854
didiegovieira <diego.vieira@sptech.school>
2022-11-14 15:31:20 -0300

    att all

--- site/public/js/register.js
+++ site/public/js/register.js
@@ -4,3 +4,30 @@ function sumirMensagem() {
     // cardErro.style.display = "none"
 }
 
+
+/*     function prosseguir1() {
+        var displayCont = document.querySelector(".container")
+
+        displayCont.classList.toggle('ativo')
+
+        var displayCont2 = document.querySelector(".container2")
+
+        displayCont2.classList.toggle('ativo')
+    }
+ */
+
+    function prosseguir() {
+        box1.style.display = "none"
+        box2.style.display = "flex"
+
+        var displayCont = document.querySelector(".container")
+
+        displayCont.classList.toggle('ativo')
+    }
+
+    function voltar() {
+        box1.style.display = "flex"
+        box2.style.display = "none"
+
+        
+    }
