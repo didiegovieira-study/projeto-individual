0504f8b528b41b3f6666337a892fe9e8e11024b4
didiegovieira <diego.vieira@sptech.school>
2022-11-14 16:27:57 -0300

    att register

--- site/public/js/register.js
+++ site/public/js/register.js
@@ -1,33 +1,84 @@
 
-
-function sumirMensagem() {
-    // cardErro.style.display = "none"
-}
-
-
-/*     function prosseguir1() {
-        var displayCont = document.querySelector(".container")
-
-        displayCont.classList.toggle('ativo')
-
-        var displayCont2 = document.querySelector(".container2")
-
-        displayCont2.classList.toggle('ativo')
-    }
- */
-
     function prosseguir() {
         box1.style.display = "none"
         box2.style.display = "flex"
 
-        var displayCont = document.querySelector(".container")
-
-        displayCont.classList.toggle('ativo')
     }
 
     function voltar() {
         box1.style.display = "flex"
         box2.style.display = "none"
+    }
 
-        
+    function selecao() {
+        var sel = document.getElementById('selSelecao');
+        var img = document.getElementById('image');
+
+        if (sel == '') {
+            img.src = ''
+        } else if (sel == 1) {
+            img.src = './assets/img/selecao/1.png'
+        } else if (sel == 2) {
+            img.src = './assets/img/selecao/2.png'
+        } else if (sel == 3) {
+            img.src = './assets/img/selecao/3.png'
+        } else if (sel == 4) {
+            img.src = './assets/img/selecao/4.png'
+        } else if (sel == 5) {
+            img.src = './assets/img/selecao/5.png'
+        } else if (sel == 6) {
+            img.src = './assets/img/selecao/6.png'
+        } else if (sel == 7) {
+            img.src = './assets/img/selecao/7.png'
+        } else if (sel == 8) {
+            img.src = './assets/img/selecao/8.png'
+        } else if (sel == 9) {
+            img.src = './assets/img/selecao/9.png'
+        } else if (sel == 10) {
+            img.src = './assets/img/selecao/10.png'
+        } else if (sel == 11) {
+            img.src = './assets/img/selecao/11.png'
+        } else if (sel == 12) {
+            img.src = './assets/img/selecao/12.png'
+        } else if (sel == 13) {
+            img.src = './assets/img/selecao/13.png'
+        } else if (sel == 14) {
+            img.src = './assets/img/selecao/14.png'
+        } else if (sel == 15) {
+            img.src = './assets/img/selecao/15.png'
+        } else if (sel == 16) {
+            img.src = './assets/img/selecao/16.png'
+        } else if (sel == 17) {
+            img.src = './assets/img/selecao/17.png'
+        } else if (sel == 18) {
+            img.src = './assets/img/selecao/18.png'
+        } else if (sel == 19) {
+            img.src = './assets/img/selecao/19.png'
+        } else if (sel == 20) {
+            img.src = './assets/img/selecao/20.png'
+        } else if (sel == 21) {
+            img.src = './assets/img/selecao/21.png'
+        } else if (sel == 22) {
+            img.src = './assets/img/selecao/22.png'
+        } else if (sel == 23) {
+            img.src = './assets/img/selecao/23.png'
+        } else if (sel == 24) {
+            img.src = './assets/img/selecao/24.png'
+        } else if (sel == 25) {
+            img.src = './assets/img/selecao/25.png'
+        } else if (sel == 26) {
+            img.src = './assets/img/selecao/26.png'
+        } else if (sel == 27) {
+            img.src = './assets/img/selecao/27.png'
+        } else if (sel == 28) {
+            img.src = './assets/img/selecao/28.png'
+        } else if (sel == 29) {
+            img.src = './assets/img/selecao/29.png'
+        } else if (sel == 30) {
+            img.src = './assets/img/selecao/30.png'
+        } else if (sel == 31) {
+            img.src = './assets/img/selecao/31.png'
+        } else if (sel == 32) {
+            img.src = './assets/img/selecao/32.png'
+        }
     }
