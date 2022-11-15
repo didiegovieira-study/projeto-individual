c149e6895653332fa027564e7bbcc67aff8e9a87
didiegovieira <diego.vieira@sptech.school>
2022-11-15 00:30:43 -0300

    att all

---
+++ site/public/js/home.js
@@ -0,0 +1,274 @@
+
+    function voltar() {
+        boxNull.style.display = 'block'
+        box.style.display = 'none'
+        imgExit.style.display = 'none'
+    }
+    
+    function copa1930() {
+
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br>
+        <h1>1930</h1>
+        <p>O <b>Uruguai</b> sediou a primeira Copa do  Mundo e as 18 partidas foram feitas somente na capital Montevidéu. O time uruguaio foi à final contra a Argentina e venceu os vizinhos por 4 a 2. No dia seguinte, foi feriado no país para celebrar a conquista do título no primeiro campeonato.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+
+    }
+
+    function copa1934() {
+
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br>
+        <h1>1934</h1>
+        <p>A <b>Itália</b> sediou a segunda Copa do Mundo e também se tornou campeã em casa, vencendo a antiga Tchecoslováquia (atual República Tcheca e Eslováquia) na prorrogação por 2 a 1. Os jogos foram transmitidos ao vivo pelo rádio para 12 países. 
+        <br>
+        O campeão Uruguai não disputou a Copa em retaliação à Itália, por ter se recusado a viajar e participar da primeira edição.</p>`
+        
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa1938() {
+
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1938</h1>
+        <p>A <b>Itália se tornou bicampeã</b> ao vencer a Hungria por 4 a 2. O Brasil foi o terceiro colocado. Pela primeira vez, um país asiático participou da Copa: as Índias Orientais Holandesas (Indonésia).</p>`
+        
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa1950() {
+
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1950</h1>
+        <p>Depois de um intervalo com as copas de 1942 e 1946 canceladas por causa da Segunda Guerra Mundial, a retomada foi no Brasil.
+        <br>
+        Jogando em casa, a equipe brasileira conseguiu chegar até a final, que foi disputada no Maracanã, no Rio de Janeiro, considerado o maior estádio do mundo na época. No entanto, o <b>Uruguai</b> conseguiu virar o jogo e se tornou <b>bicampeão</b> por 2 a 1.</p>`
+        
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa1954() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1954</h1>
+        <p>A Copa de 1954 marca o retorno da Alemanha, dessa vez como Alemanha Ocidental, à Copa desde o final da Segunda Guerra. Mesmo não sendo a favorita, <b>os alemães venceram a Hungria</b> de virada, por 3 a 2, marcando o gol da vitória já no fim da partida.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+    function copa1958() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1958</h1>
+        <p>A Copa de 1958 tinha uma dupla invencível de atacantes: Pelé e Garrincha, que levaram o Brasil à final, que venceu a anfitriã dos jogos, a Suécia, por 5 a 2. <b>O Brasil foi o primeiro país a conquistar o título em um continente diferente</b>.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa1962() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1962</h1>
+        <p>Mais uma vez na final, <b>o Brasil venceu a Tchecoslováquia por 3 a 1</b> na Copa no Chile. Garrincha estava suspenso da partida por conta de um cartão vermelho na semifinal. Mas até o presidente do Chile, Jorge Alessandri, solicitou que o atacante brilhasse na final e o pedido foi aceito.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa1966() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1966</h1>
+        <p>A <b>Inglaterra venceu a Alemanha Ocidental</b> por 4 a 2 na prorrogação. A Rainha Elizabeth II estava no estádio em Wembley e presenciou o país conquistar seu primeiro e único título até o momento.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa1970() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1970</h1>
+        <p><b>O Brasil se tornou tricampeão</b>, vencendo a Itália por 4 a 1. O time brasileiro se consagrou no futebol mundial com jogadores como Pelé, Rivelino, Tostão e Carlos Alberto.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+    
+    function copa1974() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1974</h1>
+        <p>A <b>Alemanha Ocidental venceu a Holanda em casa</b> por 2 a 1. A Copa foi marcada pela atuação marcante do jogador holandês Johan Cruyff e do alemão Franz Beckenbauer. </p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+    function copa1978() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1978</h1>
+        <p>Em sua segunda final consecutiva, a Holanda perdeu novamente, desta vez para a dona da casa, a <b>Argentina</b>. Passaram-se 48 anos desde a primeira Copa do Mundo até que os argentinos levassem o título por 3 a 1.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa1982() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1982</h1>
+        <p>Depois de  uma semifinal disputada nos pênaltis entre Alemanha Ocidental e França, a seleção alemã venceu, mas não conquistou a taça Jules Rimet. <b>A Itália venceu os alemães por 3 a 1</b> e se igualou ao Brasil com o tricampeonato na Copa da Espanha.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa1986() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1986</h1>
+        <p>A Copa do México foi do argentino Diego Maradona, que estava no auge de sua carreira e contribuiu para vencer a Alemanha Ocidental. <b>A Argentina conquistou o título pela segunda vez</b>.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa1990() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1990</h1>
+        <p>Alemanha e Argentina se enfrentaram novamente em uma final, mas dessa vez <b>os alemães levaram a melhor por 1 a 0</b>. O gol decisivo foi de um pênalti faltando menos de cinco minutos para terminar a partida.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+    
+    function copa1994() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1994</h1>
+        <p>A final clássica entre Brasil e Itália, decidida nos pênaltis. O chute para fora do gol do italiano Roberto Baggio <b>entregou o título ao Brasil</b>, que conquistou o primeiro tetracampeonato entre os vencedores da Copa.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+    function copa1998() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>1930</h1>
+        <p>Depois de um tetra, a expectativa sobre a seleção brasileira caiu por terra. Apesar do Brasil chegar à final com a França, <b>a vitória foi de 3 a 0 para os franceses</b>.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa2002() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>2002</h1>
+        <p>Realizada pela primeira vez na Ásia e em dois países ao mesmo tempo, a Copa da Coreia do Sul e do Japão marcou também <b>a vitória do Brasil pela quinta vez</b>. A seleção brasileira venceu a Alemanha por 2 a 0.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa2006() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>2006</h1>
+        <p>Com semifinais e uma final composta apenas de países europeus, <b>a Itália venceu a França nos pênaltis</b> e se consagrou tetracampeã.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa2010() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>2010</h1>
+        <p>Pela primeira vez a ser realizada no continente africano, a Copa na África do Sul também teve um estreante entre as seleções campeãs do mundo. A <b>Espanha</b>, que até então nunca havia passado das quartas de final, venceu a Holanda por 1 a 0 na prorrogação.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+    
+    function copa2014() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>2014</h1>
+        <p>Na segunda Copa do Mundo no Brasil, a final ficou entre a Alemanha e a Argentina. A partida foi decidida só na prorrogação, que deu <b>vitória aos alemães por 1 a 0</b>, sendo a primeira seleção europeia a ganhar o torneio no continente americano.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa2018() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>2018</h1>
+        <p><b>A França conquistou o bicampeonato na final contra a Croácia</b>, vencendo por 4 a 2. O francês Kylian Mbappe foi o segundo jogador mais novo a disputar uma final, com 19 anos. 
+        <br>
+        O mais novo, até o momento, continua a ser o brasileiro Pelé, que jogou a final de 1958, na Suécia, com apenas 17 anos.</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
+
+    function copa2022() {
+        
+        box.innerHTML = `<img class="imgExit" id="imgExit" src="./assets/img/exit.png" alt="" style="display: none;" onclick="voltar()">
+        <br><br><br><br>
+        <h1>2022</h1>
+        <p>Até o momento não temos o vencedor, mas esperamos que nossa nação consiga conquistar o hexa!</p>`
+
+        boxNull.style.display = 'none'
+        box.style.display = 'block'
+        imgExit.style.display = 'block'
+    }
