5806042577880073f12ba8d36dcf2d7e04ae8c63
didiegovieira <diego.vieira@sptech.school>
2022-11-16 22:27:15 -0300

    att all

--- site/app.js
+++ site/app.js
@@ -11,7 +11,7 @@ var app = express();
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var selecaoRouter = require("./src/routes/selecao");
-var medidasRouter = require("./src/routes/medidas");
+// var medidasRouter = require("./src/routes/medidas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,7 +22,7 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/selecao", selecaoRouter);
-app.use("/medidas", medidasRouter)
+// app.use("/medidas", medidasRouter)
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
